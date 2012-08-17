7d1f97971b2cbdf6033c3cffea786dfff55aacc1
chrissbarnett <chrissbarnett@gmail.com>
2012-08-17 16:00:27 -0400

    BugFix:  Spring is unable to inject a bean into the HttpSessionListener in the usual way, so a NullPointerException was being thrown when session destroy tried to access the IngestStatusManager.  Found a different way to reference this bean.

--- ogpIngest/src/main/java/org/OpenGeoPortal/Ingest/IngestStatusSessionListener.java
+++ ogpIngest/src/main/java/org/OpenGeoPortal/Ingest/IngestStatusSessionListener.java
@@ -1,26 +1,36 @@
 package org.OpenGeoPortal.Ingest;
 
+import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 public class IngestStatusSessionListener implements HttpSessionListener {
-@Autowired
-private IngestStatusManager ingestStatusManager;
+
 final Logger logger = LoggerFactory.getLogger(this.getClass());
 
    public void sessionDestroyed(HttpSessionEvent httpSessionEvent) {
-	   String sessionId = httpSessionEvent.getSession().getId();
-	   ingestStatusManager.removeStatusBySessionId(sessionId);
-	   logger.info("Removing Status info for session: "+ sessionId);
+	   HttpSession session = httpSessionEvent.getSession();
+	   String sessionId = session.getId();
+	   //unfortunately, there doesn't currently seem to be a nice way to inject this dependency in the regular fashion
+       ApplicationContext ctx = WebApplicationContextUtils.getWebApplicationContext(session.getServletContext());
+       IngestStatusManager ingestStatusManager = (IngestStatusManager) ctx.getBean("ingestStatusManager");
+	   if (ingestStatusManager != null){
+		   ingestStatusManager.removeStatusBySessionId(sessionId);
+		   logger.debug("Removing Status info for session: "+ sessionId);
+	   } else {
+		   logger.error("The ingestStatusManager has a null value here");
+	   }
    }
-
+ 
+	
 public void sessionCreated(HttpSessionEvent httpSessionEvent) {
 	// not doing anything here for now
-	
+	logger.info("new session created");
 }	
 
 
